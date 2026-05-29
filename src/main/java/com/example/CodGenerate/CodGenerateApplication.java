03b89d0eef0ee0c64a7e6fbb3bdfde490e0e3e6e
AM <armen.melkonyan.93@inbox.ru>
2026-05-29 18:07:50 +0300

    initializing project

---
+++ src/main/java/com/example/CodGenerate/CodGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodGenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodGenerateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodGenerateApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/CodGenerate/CodGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodGenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodGenerateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
